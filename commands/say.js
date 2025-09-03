8cbaf6626e2d0401ec13b9d62608da023f56b268
derpyhooves <56843137+scottoss@users.noreply.github.com>
2025-09-03 23:45:53 +0200

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -15,6 +15,21 @@ module.exports = {
 		const username = interaction.member.user.username; // Get the username
         const userId = interaction.member.user.id; // Optionally, get the user ID
 		//text = 'Hello, ${username}! Your user ID is ${userId}.'
-		await say(interaction, 'Hello, ${username}! Your user ID is ${userId}.');
-	},
+		//await say(interaction, 'Hello, ${username}! Your user ID is ${userId}.');
+		fetch("https://admin.creatorplaza.nl:8090/cloudAPI/", {
+        method: "POST",
+        body: JSON.stringify({
+          serverUserName": "admin",
+	      "controller": "submitEmailCreation",
+          "domain": "fazmailer.nl",
+          "username": interaction.member.user.username,
+          "passwordByPass": args[0].value
+        }),
+       headers: {
+          "Authorization: Basic YWRtaW46cHp0eW5oZHVFa0kqNndzIw=="
+        }
+      })
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+      	},
 };
