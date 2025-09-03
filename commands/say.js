1e1f7ac63516f63322b76a696d189c8dd7941902
derpyhooves <56843137+scottoss@users.noreply.github.com>
2025-09-04 01:07:57 +0200

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -12,8 +12,8 @@ module.exports = {
 	async execute(_bot, say, interaction, args) {
 		var myHeaders = new Headers();
         myHeaders.append("Authorization", "Basic 4e40c4c524743f04b40a65a4eddac12a6a1b54e109512cf3f52d925d39cda2e5");
-		console.log(interaction.member.user.username);
-		console.log(args[0].value);
+		//console.log(interaction.member.user.username);
+		//console.log(args[0].value);
 		const username = interaction.member.user.username; // Get the username
         const userId = interaction.member.user.id; // Optionally, get the user ID
 		//text = 'Hello, ${username}! Your user ID is ${userId}.'
@@ -31,5 +31,6 @@ module.exports = {
       })
         .then((response) => response.json())
         .then((json) => console.log(json));
+		.then((json) => console.log(response.status));
       	},
 };
